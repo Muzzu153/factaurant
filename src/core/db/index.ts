058947514115674aedf56afcc69dc0d4eb4d17c8
Muzzu153 <muzzusyed153@gmail.com>
2026-01-03 17:48:32 +0530

    Define db schema and initiate connection

--- src/core/db/index.ts
+++ src/core/db/index.ts
@@ -1,5 +1,15 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
+import {Pool} from 'pg'
+import * as schema from './schema'
 
-import * as schema from './schema.ts'
+const connectionString = process.env.DATABASE_URL;
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema })
+if(!connectionString){
+    console.log(connectionString)
+    // throw new Error('X Database URL is missing in .env')
+}
+
+
+const pool = new Pool({connectionString})
+
+export const db = drizzle(pool, {schema})
--- src/core/db/schema.ts
+++ src/core/db/schema.ts
@@ -1,7 +1,80 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { pgTable, text, uuid, jsonb, timestamp, integer, boolean } from 'drizzle-orm/pg-core'
 
-export const todos = pgTable('todos', {
-  id: serial().primaryKey(),
-  title: text().notNull(),
+// ----- 1. THE TENANT TABLE (The  "Shop Owner") -----
+export const tenants = pgTable('tenants', {
+  // We use UUIDs (random strings) instead of Numbers (1, 2, 3)
+  // because they are harder to guess. You don't want someone accessing /tenant/4.
+  id: uuid().defaultRandom().primaryKey(),
+
+
+  // This is the subdomain like ("pizza-King ")
+  // We index it because we look it up on every request.
+  slug: text('slug').unique().notNull(),
+
+  // The visible name "Pizza King Inc"
+  name: text('name').notNull(),
+
+  // Theme config (The factory logic)
+  // Instead of hardcoding colors, we store them as JSON.
+  // The frontend will read this and apply CSS variables.
+  theme: jsonb('theme').$type<{
+    // Colours
+    primaryColor: string; // Main brand color
+    background: string; // hex code (e.g. #ffffff or #0f0f0f for dark mode)
+
+    // Geometry (The "Vibe")
+    radius: string;  // "0rem" (Sharp), "0.5rem" (Standard), "2rem" (Round)
+
+    // typography
+    fontHeading: string; // "Inter", "Playfair Display", "Space Mono"
+    fontBody: string;
+  }>().notNull(),
+
+  // the layout sytem 
+  layout: jsonb('layout').$type<{
+    navbar: 'simple' | 'centered' | 'hamburger';
+    hero: 'simple-text' | 'split-image' | 'video-bg';
+    productCard: 'minimal' | 'shadow' | 'bordered';
+  }>().notNull()
+
+})
+
+// ---- 2. THE PRODUCT TABLE (The "Menu Itme") ----
+export const products = pgTable('products',{
+  id: uuid('id').defaultRandom().primaryKey(),
+
+  // The Golden Rule
+  // Every product MUST belong to a tenant
+  // We use references() to enforce this relationship at the databse level.
+  tenantId: uuid('tenantId').references(()=> tenants.id).notNull(),
+
+  name: text('name').notNull(),
+  description: text('description'),
+
+  // We store money in integers to avoid floating point math errors.
+  // 10.99 => 10.99
+  price: integer('price').notNull(),
+
+  imageUrl: text('image-url'),
+  isAvailable: boolean('is_available').default(true).notNull(),
+})
+
+
+export const orders = pgTable('orders', {
+  id: uuid('id').defaultRandom().notNull(),
+  tenantId: uuid('tenantId').references(()=>tenants.id).notNull(),
+
+  // Who bought it? Guest checkout for MVP
+  custormerName: text('customer_name').notNull(),
+  customerAddress: text('customer_address').notNull(),
+
+
+  // What did they buy?
+  // Storing the cart items as a JSON blob for simplicity
+  // In a huge app, "order_items" will be required, but jSON is fine for now.
+  items: jsonb('items').notNull(),
+
+  totalAmount: integer('total_amount').notNull(),
+  status: text('status').default('pending'), // pending, cooking, devlivered
   createdAt: timestamp('created_at').defaultNow(),
 })
