9cc9cf2da8d3cb6bc721103179d9f399d617db33
Muzzu153 <muzzusyed153@gmail.com>
2026-01-09 01:31:37 +0530

    Create: two server functions
    create two server functions wrappers, one for tenant resolution and other for queries

---
+++ src/core/runtime/serverFns.ts
@@ -0,0 +1,18 @@
+import { createServerFn } from "@tanstack/react-start";
+import { tenantMiddleware } from "@/modules/tenant/tenant.middleware";
+
+/**
+ * PUBLIC FUNCTIONS
+ * - No tenant
+ * - No scoped DB
+ * - Used for bootstrap, previews, landing
+ */
+export const publicFn = createServerFn({ method: "GET" });
+
+/**
+ * TENANT FUNCTIONS
+ * - Tenant already resolved
+ * - context.db is guaranteed
+ */
+export const tenantFn = createServerFn({ method: "GET" })
+  .middleware([tenantMiddleware]);
