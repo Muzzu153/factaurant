dbfcb92db1bafd7be7183e7a47f9d89ec182a494
Muzzu153 <muzzusyed153@gmail.com>
2026-01-08 18:04:15 +0530

    Update: db schema for component registry & theme
    Add schema for ui blocks and page layout
    Update the schema for brand theme, categorize them and add more styles

--- src/core/db/schema.ts
+++ src/core/db/schema.ts
@@ -1,5 +1,24 @@
 import { pgTable, text, uuid, jsonb, timestamp, integer, boolean } from 'drizzle-orm/pg-core'
 
+// 1. Define TYPES FOR TYPESCRIPT INTELLISENSE
+export type Block = {
+  type: string,
+  props: Record<string, any>
+}
+
+export type PageLayout = {
+  layout: {
+    header: string[];
+    main: string[];
+    footer: string[];
+    siderBar?: string[],
+  },
+  meta?: {
+    title: string,
+    description: string,
+  },
+}
+
 // ----- 1. THE TENANT TABLE (The  "Shop Owner") -----
 export const tenants = pgTable('tenants', {
   // We use UUIDs (random strings) instead of Numbers (1, 2, 3)
@@ -19,34 +38,60 @@ export const tenants = pgTable('tenants', {
   // The frontend will read this and apply CSS variables.
   theme: jsonb('theme').$type<{
     // Colours
-    primaryColor: string; // Main brand color
-    background: string; // hex code (e.g. #ffffff or #0f0f0f for dark mode)
+    colors: {
+      primary: string; // Main brand color
+      secondary: string,
+      tertiary: string,
+      accent: string,
+      text: string,
+      background: string; // hex code (e.g. #ffffff or #0f0f0f for dark mode)
+    }
 
     // Geometry (The "Vibe")
-    radius: string;  // "0rem" (Sharp), "0.5rem" (Standard), "2rem" (Round)
+    borderRadius: {
+      xsm: string,
+      sm: string,
+      md: string,
+      lg: string,
+      xl: string,
+    }
 
     // typography
-    fontHeading: string; // "Inter", "Playfair Display", "Space Mono"
-    fontBody: string;
+    fonts: {
+      heading: string; // "Inter", "Playfair Display", "Space Mono"
+      body: string;
+    },
+
+
+
   }>().notNull(),
 
+  // 2. THE HEADLESS CMS COLUMNS
+  // Inventory: All blocks available for this tenant
+  // Reusable blocks
+  blocks: jsonb('blocks').$type<Record<string, Block>>().notNull(),
+
+ // Blueprint: How blocks are arranged on pages
+//  Pages reference blocks 
+  pages: jsonb('pages').$type<Record<string, PageLayout>>().notNull(),
+
   // the layout sytem 
-  layout: jsonb('layout').$type<{
-    navbar: 'simple' | 'centered' | 'hamburger';
-    hero: 'simple-text' | 'split-image' | 'video-bg';
-    productCard: 'minimal' | 'shadow' | 'bordered';
-  }>().notNull()
+  // layout: jsonb('layout').$type<{
+  //   navbar: 'simple' | 'centered' | 'hamburger';
+  //   hero: 'simple-text' | 'split-image' | 'video-bg';
+  //   productCard: 'minimal' | 'shadow' | 'bordered';
+  // }>().notNull()
 
 })
 
 // ---- 2. THE PRODUCT TABLE (The "Menu Itme") ----
-export const products = pgTable('products',{
+export const products = pgTable('products', {
   id: uuid('id').defaultRandom().primaryKey(),
 
   // The Golden Rule
   // Every product MUST belong to a tenant
   // We use references() to enforce this relationship at the databse level.
-  tenantId: uuid('tenantId').references(()=> tenants.id).notNull(),
+  tenantId: uuid('tenantId').references(() => tenants.id).notNull(),
 
   name: text('name').notNull(),
   description: text('description'),
@@ -60,9 +105,19 @@ export const products = pgTable('products',{
 })
 
 
+// 1. DEFINE THE SHAPE OF THE JSON
+// This creates a contract for what goes inside the 'items' blob.
+export type OrderItemSnapshot = {
+  productId: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+
 export const orders = pgTable('orders', {
   id: uuid('id').defaultRandom().notNull(),
-  tenantId: uuid('tenantId').references(()=>tenants.id).notNull(),
+  tenantId: uuid('tenantId').references(() => tenants.id).notNull(),
 
   // Who bought it? Guest checkout for MVP
   custormerName: text('customer_name').notNull(),
@@ -72,9 +127,9 @@ export const orders = pgTable('orders', {
   // What did they buy?
   // Storing the cart items as a JSON blob for simplicity
   // In a huge app, "order_items" will be required, but jSON is fine for now.
-  items: jsonb('items').notNull(),
+  items: jsonb('items').$type<OrderItemSnapshot[]>().notNull(),
 
   totalAmount: integer('total_amount').notNull(),
-  status: text('status').default('pending'), // pending, cooking, devlivered
+  status: text('status').default('pending').notNull(), // pending, cooking, devlivered
   createdAt: timestamp('created_at').defaultNow(),
 })
