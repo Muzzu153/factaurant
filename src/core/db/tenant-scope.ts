37e27920ada12bb2c997dfd0753386fef9515286
Muzzu153 <muzzusyed153@gmail.com>
2026-01-18 16:36:54 +0530

    Create fn that wraps tenantId with db queries

--- src/core/db/tenant-scope.ts
+++ src/core/db/tenant-scope.ts
@@ -1,5 +1,5 @@
 // app/core/db/tenant-scope.ts
-import { eq, desc } from 'drizzle-orm';
+import { eq, desc, and } from 'drizzle-orm';
 import { db } from './client';
 import { products, orders, tenants } from './schema';
 
@@ -12,9 +12,9 @@ export type TenantScope = ReturnType<typeof createTenantScope>;
  * Never import or use this in tenant resolution logic.
  */
 export function createTenantScope(tenantId: string) {
-    if(!tenantId){
-        throw new Error("Tenant ID is required")
-    }
+  if (!tenantId) {
+    throw new Error("Tenant ID is required")
+  }
 
   return {
     // 🍕 PRODUCT REPOSITORY
@@ -22,46 +22,126 @@ export function createTenantScope(tenantId: string) {
       // 1. SAFE READ
       // Developer doesn't need to pass tenantId. It's baked in.
       findMany: async () => {
-        return db
+        const allProducts = await db
           .select()
           .from(products)
           .where(eq(products.tenantId, tenantId));
+
+        return allProducts
       },
-      
+
       // 2. SAFE WRITE
       // Developer passes data, we inject the tenantId automatically.
-      create: async (data: typeof products.$inferInsert) => {
-        return db.insert(products).values({
-          ...data,
-          tenantId: tenantId, // 🔒 FORCED OVERRIDE
-        }).returning();
-      }
+      create: async (data: Omit<typeof products.$inferInsert, "tenantId">) => {
+        const [created] = await db
+          .insert(products)
+          .values({
+            ...data,
+            tenantId: tenantId, // FORCED OVERRIDE
+          }).returning();
+
+        return created
+      },
+
+      update: async (id: string, data: Partial<Omit<typeof products.$inferInsert, "tenantId" | "id" | "createdAt">>) => {
+        const updated = await db
+          .update(products)
+          .set({
+            ...data,
+            tenantId: tenantId  // FORCED OVERRIDE
+          })
+          .where(
+            and(
+              eq(products.id, id),
+              eq(products.tenantId, tenantId),
+            )
+          )
+          .returning()
+
+        return updated
+      },
+
+      delete: async (id: string) => {
+        const [deleted] = await db
+          .delete(products)
+          .where(
+            and(
+              eq(products.id, id),
+              eq(products.tenantId, tenantId),
+            )
+          )
+          .returning()
+
+        return deleted
+      },
+
     },
 
     // 🛒 ORDER REPOSITORY
     orders: {
       findMany: async () => {
-        return db
+        const allOrder = await db
           .select()
           .from(orders)
           .where(eq(orders.tenantId, tenantId))
           .orderBy(desc(orders.createdAt));
+
+        return allOrder
       },
-      
+
       create: async (data: Omit<typeof orders.$inferInsert, 'tenantId'>) => {
-        return db.insert(orders).values({
-          ...data,
-          tenantId: tenantId, // 🔒 FORCED OVERRIDE
-        }).returning();
-      }    
+        const [created] = await db
+          .insert(orders)
+          .values({
+            ...data,
+            tenantId: tenantId, // 🔒 FORCED OVERRIDE
+          }).returning();
+
+        return created
+      },
+
+      updateStatus: async (id: string, status: string) => {
+        const updated = await db
+          .update(orders)
+          .set({ status, })
+          .where(
+            and(
+              eq(orders.id, id),
+              eq(orders.tenantId, tenantId),
+            )
+          )
+          .returning()
+
+        return updated
+      },
+
+      delete: async (id: string) => {
+        const deleted = await db
+          .delete(orders)
+          .where(
+            and(
+              eq(orders.id, id),
+              eq(orders.tenantId, tenantId),
+            )
+          )
+          .returning()
+
+        return deleted
+      },
+
     },
 
     self: {
-        get: async () => {
-            return db.query.tenants.findFirst({
-                where: eq(tenants.id, tenantId)
-            })
-        }
+      get: async () => {
+        const tenant = await db
+          .query
+          .tenants
+          .findFirst({
+            where: eq(tenants.id, tenantId)
+          })
+
+        return tenant
+      }
     }
   };
 }
