2f6882db244adb77e7ae68fe083fb059f6548e14
Muzzu153 <muzzusyed153@gmail.com>
2026-02-08 19:41:05 +0530

    feat(env): implement type safe env with zod
    - Add ServerEnv for for database and deployment configuration
    - Implement runtime parsing to ensure required variables exist

---
+++ src/core/runtime/env.server.ts
@@ -0,0 +1,13 @@
+// core/runtime/env.ts
+import { z } from 'zod';
+
+const ServerEnvSchema = z.object({
+  APP_DEPLOYMENT_MODE: z.enum(['shared', 'single']).default('shared'),
+  SINGLE_TENANT_ID: z.uuid().optional(),
+
+  DATABASE_URL: z.url(),
+
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+});
+
+export const env = ServerEnvSchema.parse(process.env);
