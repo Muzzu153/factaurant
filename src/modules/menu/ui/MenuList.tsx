facc163f03e1adadcb3832a3b31b6776c501f5e6
Muzzu153 <muzzusyed153@gmail.com>
2026-02-09 16:39:19 +0530

    refactor: format document & use relative imports to fix Vitest resolution
    - Replace alias path(@) with relative paths(..) to ensure Vitest can correctly parse and resolve imports during testing.
    - Format document with prettier.

--- src/modules/menu/ui/MenuList.tsx
+++ src/modules/menu/ui/MenuList.tsx
@@ -1,41 +1,45 @@
-// MenuList.tsx
-import { useQuery } from '@tanstack/react-query';
-import { getMenu } from '../menu.server';
-import { cartService } from '@/modules/cart/cart.store';
-// import { MenuItem } from './types';
+import { useQuery } from '@tanstack/react-query'
+import { getMenu } from '../menu.server'
+import { cartService } from '../../cart/cart.store'
 
-// interface MenuListProps {
-//   items: MenuItem[];
-// }
+export function MenuList() {
+  const { data: products } = useQuery({
+    queryKey: ['menu'],
+    queryFn: () => getMenu(), // Server Function
+  })
 
-
-export function MenuList( ) {
-
-     const { data: products } = useQuery({
-        queryKey: ['menu'],
-        queryFn: () => getMenu(), // Server Function
-      });
-
-      if(!products) return <div>Loading Menu...</div>
+  if (!products) return <div>Loading Menu...</div>
 
   return (
     <div className="menu-container">
       <h2>Our Menu</h2>
       <ul style={{ listStyle: 'none', padding: 0 }}>
         {products.map((item) => (
-          <li key={item.id} style={{ marginBottom: '20px', borderBottom: '1px solid #ccc' }} >
+          <li
+            key={item.id}
+            style={{ marginBottom: '20px', borderBottom: '1px solid #ccc' }}
+          >
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
               <strong>{item.name}</strong>
               <span>${item.price.toFixed(2)}</span>
-              <button onClick={()=>cartService.add(item)} className='bg-(--accent) text-(--text) py-1 px-3'>Add</button>
+              <button
+                onClick={() => cartService.add(item)}
+                className="bg-(--accent) text-(--text) py-1 px-3"
+              >
+                Add
+              </button>
             </div>
-            <p style={{ fontSize: '0.9em', color: '#666' }}>{item.description}</p>
-            <em style={{ fontSize: '0.8em' }} className='text-blue-400'>{item.isAvailable}</em>
+            <p style={{ fontSize: '0.9em', color: '#666' }}>
+              {item.description}
+            </p>
+            <em style={{ fontSize: '0.8em' }} className="text-blue-400">
+              {item.isAvailable}
+            </em>
           </li>
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default MenuList;
+export default MenuList
